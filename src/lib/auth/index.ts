043c32c0a1e2ba8df139267c0c362a9864361514
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2025-08-21 23:08:24 -0400

    init auth

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,56 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+export const auth = betterAuth({
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL
+  }),
+  socialProviders: {
+    spotify: {
+      clientId: process.env.SPOTIFY_CLIENT_ID as string,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+    },
+  },
+  user: {
+    modelName: "users",
+    fields: {
+      emailVerified: "email_verified",
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  },
+  session: {
+    modelName: "sessions",
+    fields: {
+      userId: "user_id",
+      ipAddress: "ip_address",
+      userAgent: "user_agent",
+      expiresAt: "expires_at",
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  },
+  account: {
+    modelName: "acounts",
+    fields: {
+      userId: "user_id",
+      accountId: "account_id",
+      providerId: "provider_id",
+      accessToken: "access_token",
+      refreshToken: "refresh_token",
+      accessTokenExpiresAt: "access_token_expires_at",
+      refreshTokenExpiresAt: "refresh_token_expires_at",
+      idToken: "id_token",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  },
+  verification: {
+    modelName: "verifications",
+    fields: {
+      expiresAt: "expires_at",
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+});
